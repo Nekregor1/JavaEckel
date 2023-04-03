0e9352117fb13b201fe248f07a5a4c5d0cf872e0
Nekregor <nekrasov.e.g@yandex.ru>
2023-04-03 23:03:58 +0300

    Доработал документирование. Начал делать упражнения

--- Lesson2prog1/src/HelloDate.java
+++ Lesson2prog1/src/HelloDate.java
@@ -1,6 +1,17 @@
-// HelloDate.java
+//: object/HelloDate.java
 import java.util.*;
+
+/** Первая программа-пример книги.
+ * Выводит строку и текущую дату.
+ * @author Брюс Эккель
+ * @author Nekregor
+ * @version 4.0
+ */
 public class HelloDate {
+    /** Точка входа в класс и приложение
+     * @param args Массив строковых аргументов
+     * @throws exceptions Исключения не выдаются
+     */
     public static void main(String[] args) {
         System.out.println("Привет, сегодня: ");
         System.out.println(new Date());
@@ -8,4 +19,7 @@ public class HelloDate {
         System.out.println(System.getProperty("user.name"));
         System.out.println(System.getProperty("java.library.path"));
     }
-}
+} /* Output: (55% match)
+Привет, сегодня:
+Wed Oct 05 14:39:36 MDT 2005
+*///:~
---
+++ Lesson2prog1/src/exercise1_1.java
@@ -0,0 +1,11 @@
+public class exercise1_1 {
+    int i;
+    char ch;
+    public exercise1_1() {
+        System.out.println("i = " + i);
+        System.out.println("ch = " + ch);
+    }
+    public static void main(String[] args) {
+        new exercise1_1();
+    }
+}
